684a341a8e33141a4270b493610d068bcf5e33ef
Khushal rajoria <rajoriakhushal@gmail.com>
2025-01-17 17:27:09 +0530

    new apporach

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter(); // Initialize Hive for Flutter.
-  Hive.registerAdapter(StreakAdapter()); // Register the Streak adapter.
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Streak App',
+      title: 'Flutter Demo',
       theme: ThemeData(
+       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Streak App')),
-        body: const Center(child: Text('Streak App Home')),
-      ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
